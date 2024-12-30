f75189c2d0cd5289ec7fd344fd161db597c75184
vineethm5 <1609567@sdmcujire.in>
2024-12-30 18:11:33 +0530

    added dotenv

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const express =  require('express');
 const errorhandler = require('./Middleware/errorHandler');
 const connectDb = require("./config/dbConnection");
 const app = express();
 
-const PORT= process.env.port || 3030;
+const PORT= process.env.PORT || 3030;
 connectDb();
 app.use(express.json());
 app.use("/contact/api",require("./router/contactRouter"));
