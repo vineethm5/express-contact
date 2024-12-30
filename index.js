9a6a2c51fa46fe82331d0acfdfec496df94a4a8c
vineethm5 <1609567@sdmcujire.in>
2024-12-30 12:48:36 +0530

    30 monday

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const errorhandler = require('./Middleware/errorHandler');
 const connectDb = require("./config/dbConnection");
 const app = express();
 
-const PORT=3030;
+const PORT= process.env.port || 3030;
 connectDb();
 app.use(express.json());
 app.use("/contact/api",require("./router/contactRouter"));
 app.use(errorhandler);
 
 app.listen(PORT,(err)=>{
-    if(!err) console.log("Connected Successfully")
+    if(!err) console.log(`Connected Successfully to the ${PORT}`)
 })
