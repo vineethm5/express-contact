1210c1afef58d68c152148c8c6b16235ed038818
vineethm5 <1609567@sdmcujire.in>
2025-01-03 15:51:39 +0530

    user model created

--- Controler/userController.js
+++ Controler/userController.js
@@ -1,10 +1,23 @@
 const asyncHandler = require("express-async-handler");
-
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 //@ desc user registration
 //@ route POST user/api/register
 //@ access public
 const register= asyncHandler( async (req,res)=>
 {
+    const {username,email,password} = req.body;
+    if(!username || !email || !password)
+    {
+        res.status(400);
+        throw new Error("All fileds are mandatory");
+    }
+    const userAvailable = User.findOne({email});
+    if(!userAvailable)
+    {
+        res.status(400);
+        throw new Error("Email id is alreday exists");
+    }
     res.status(200).json({message:"user signup"});
 });
 
